7f042d34576f029c494d1a8df5317502866d51f0
outray-waleed <outray.tech2022@gmail.com>
2022-12-28 19:14:55 +0500

    Cors error fixed

--- api/index.js
+++ api/index.js
@@ -1,38 +1,31 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const authRoute=require("./routes/auth.js")
-const userRoute=require("./routes/user.js")
-const productRoute=require("./routes/product.js")
-const orderRoute=require("./routes/order.js")
-const cartRoute=require("./routes/cart.js")
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/user.js");
+const productRoute = require("./routes/product.js");
+const orderRoute = require("./routes/order.js");
+const cartRoute = require("./routes/cart.js");
+const cors = require("cors");
 
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+app.use(express.json());
 
+app.use(cors());
 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/carts", cartRoute);
 
-const dotenv = require("dotenv")
-dotenv.config()
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("DB connected"))
-.catch((err)=>{
-    console.log(err)
-})
-const app=express()
-app.use(express.json())
-
-
-
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/products",productRoute)
-app.use("/api/orders",orderRoute)
-app.use("/api/carts",cartRoute)
-
-
-
-
-
-
-
-app.listen(4555,()=>{
-    console.log("backend running")
-})
+app.listen(4555, () => {
+  console.log("backend running");
+});
--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,45 +1,44 @@
 import { useState } from "react";
 import styled from "styled-components";
 import Product from "./Product";
-import axios from 'axios';
+import axios from "axios";
 import { useEffect } from "react";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Products = ({cat,filters,sort}) => {
-  const [products,setProducts] = useState([])
-  const [filteredProducts,setFilteredProducts] = useState([])
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-  useEffect(()=>{
-   const getProducts = async () => {
-    try {
-      const res = await axios.get(
-        cat 
-        ? `http://localhost:4555/api/products?category=${cat}`
-        : "http://localhost:4555/api/products"
-      )
-      setProducts(res.data)
-    } catch (err) {
-      
-    }
-   }
-   getProducts();
-  },[cat])
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:4555/api/products?category=${cat}`
+            : "http://localhost:4555/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
 
-  useEffect (()=>{
-    setFilteredProducts(
-      products.filter((item)=>
-      Object.entries(filters).every(([key,value])=>
-      item[key].includes(value)
-      )
-      )
-    )
-  },[products,cat,filters])
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
 
   return (
     <Container>
